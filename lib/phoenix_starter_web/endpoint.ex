1c652079d2058c9fac593a937fb8c1f8d9412fc6
Ilyich Vismara <81801314+ilyichv@users.noreply.github.com>
2022-06-13 15:01:17 +0200

    feat: add cors plug for making OPTIONS work properly (#1)

--- lib/phoenix_starter_web/endpoint.ex
+++ lib/phoenix_starter_web/endpoint.ex
@@ -39,5 +39,8 @@ defmodule PhoenixStarterWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+
+  plug CORSPlug, origins: "*"
+
   plug PhoenixStarterWeb.Plug.Router
 end
