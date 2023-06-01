a2d13ceb680df89c8c7c8adbaeb2c1aeb1cba6e9
ErikFerrari <93779589+ErikFerrari@users.noreply.github.com>
2023-06-01 12:39:27 +0200

    fix: docker image (#11)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Docker entrypoint script.
 
 echo $PGPASSWORD
 echo $DB_HOSTNAME
+
 
 # Wait until Postgres is ready.
 while ! pg_isready -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER
@@ -19,12 +20,19 @@ if [[ -z `psql -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -Atqc "\\list $DB_NAME"` 
     echo "Database $DB_NAME created."
 fi
 
+
+
 SCHEMA_EXISTS=$(psql -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -t -c "SELECT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'arke_system');")
 if [ $SCHEMA_EXISTS = "f" ]; then
     echo "Schema Arke system does not exist. Creating..."
     mix arke_postgres.init_db
     # mix run priv/repo/seeds.exs
     echo "Schema Arke system created."
+fi
+
+if [ $PROJECT_ID ]; then
+    mix arke_postgres.create_project --id $PROJECT_ID
+    echo "Project $PROJECT_ID created."
 fi
 
 # if [[ -z `psql -c 'SELECT schema_name FROM information_schema.schemata WHERE schema_name =' /'/ 'arke_system;/'/ '` ]]; then
