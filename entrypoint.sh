8576d5a35ca16e33df7ee937de96a68da3442978
Ilyich Vismara <ilyich@arkemis.it>
2023-06-01 11:33:55 +0200

    chore: add github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,11 @@ if [ $SCHEMA_EXISTS = "f" ]; then
     echo "Schema Arke system created."
 fi
 
+if [ $PROJECT_ID ]; then
+    mix arke_postgres.create_project --id $PROJECT_ID
+    echo "Project $PROJECT_ID created."
+fi
+
 # if [[ -z `psql -c 'SELECT schema_name FROM information_schema.schemata WHERE schema_name =' /'/ 'arke_system;/'/ '` ]]; then
 #     echo "Schema Arke system does not exist. Creating..."
 #     mix arke_postgres.init_db
