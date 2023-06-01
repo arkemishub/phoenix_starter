02fe15c1d32aa6b840d0e8f6a6f28519df07cc4b
Erik <e.ferrari@arkemis.it>
2023-06-01 12:51:10 +0200

    chore: improve entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,27 +12,13 @@ do
   sleep 2
 done
 
-# Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -Atqc "\\list $DB_NAME"` ]]; then
-    echo "Database $DB_NAME does not exist. Creating..."
-    createdb -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -E UTF8 $DB_NAME -l en_US.UTF-8 -T template0
-    echo "Database $DB_NAME created."
-fi
-
+DB_EXISTS=$(psql -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -Atqc "\\list $DB_NAME")
 SCHEMA_EXISTS=$(psql -h $DB_HOSTNAME -p $DB_PORT -U $DB_USER -t -c "SELECT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'arke_system');")
-if [ $SCHEMA_EXISTS = "f" ]; then
-    echo "Schema Arke system does not exist. Creating..."
+
+if [ -z "$DB_EXISTS" ]  || [ "$SCHEMA_EXISTS" = "f" ] ; then
     mix arke_postgres.init_db
-    # mix run priv/repo/seeds.exs
-    echo "Schema Arke system created."
 fi
 
-# if [[ -z `psql -c 'SELECT schema_name FROM information_schema.schemata WHERE schema_name =' /'/ 'arke_system;/'/ '` ]]; then
-#     echo "Schema Arke system does not exist. Creating..."
-#     mix arke_postgres.init_db
-#     # mix run priv/repo/seeds.exs
-#     echo "Schema Arke system created."
-# fi
 
 echo "Arke is ready"
 exec mix phx.server
